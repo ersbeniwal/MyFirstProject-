9f0c3ab628b1aa4be55e60940ea4122221f9d2a3
ersbeniwal <42870223+ersbeniwal@users.noreply.github.com>
2019-11-15 13:23:22 +0530

    File Read

--- MyFirstProject/MyFirstProject.py
+++ MyFirstProject/MyFirstProject.py
@@ -1,1 +1,16 @@
+#Software Developer: Sandeep Beniwal
+
+name = ''
+while not name:
+    print('Enter your name:')
+    name = input()
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests:
+    print('Be sure to have enough room for all your guests.')
+print('Done')
+
+#File Open
+f = open("FilezillaServerLog.txt","r")
+print(f.read())
 
