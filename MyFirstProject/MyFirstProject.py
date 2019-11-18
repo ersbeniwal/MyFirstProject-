0bf0425f91c9176588a4702f928ffde87ec675ec
ersbeniwal <42870223+ersbeniwal@users.noreply.github.com>
2019-11-18 12:12:52 +0530

    Prime Number Check

--- MyFirstProject/MyFirstProject.py
+++ MyFirstProject/MyFirstProject.py
@@ -1,5 +1,25 @@
 #Software Developer: Sandeep Beniwal
 
+while(1):
+    print("Check Prime Number, Give Input")
+    x = int(input())
+    res = 0
+    i = 2
+    while(i < x):
+        if((x % i) == 0):
+            res = 1
+            break
+        i += 1
+    if(res == 0):
+        print("Prime number")
+    else:
+        print("Not Prime number")
+    
+
+
+
+
+
 name = ''
 while not name:
     print('Enter your name:')
